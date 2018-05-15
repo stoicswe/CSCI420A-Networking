3eb5b12696cd8cd2d37308d02b99e5f990c96ef8
Nathan Bunch <31823043+taranoshi@users.noreply.github.com>
2018-05-15 08:26:46 -0400

    added project2.
    This is a project that was concerned with a website hosting server and how it works.

---
+++ CircleWebHost/CircleWebHost/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+/*============================== About ==============================
+ * Author: Nathan Bunch
+ * Date: 3/11/2018
+ * Version: 1.11
+ * 
+ * Description: The Program class runs and initializes various circle
+ * sub-classes within the namespace: CircleWebHost. Eventually the
+ * webserver should be able to host more than one website at a time.
+ * 
+ * License: GNU GPL v3
+ * 
+ * Note: Made for a personal project / educational submission
+ * */
+
+/*
+ * 
+ * Changelog:
+ * 
+ * 3/11/2018 - version 1.0: generated the base code (main function)
+ * 3/11/2018 - version 1.1: beep added
+ * 3/11/2018 - version 1.11: removed beep
+ * 
+ * */
+
+namespace CircleWebHost
+{
+    class Program
+    {
+
+        private static double version = 1.11;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Welcome to Circle WebHost Ver. {version}");
+            Console.WriteLine($"Circle Master Head Ver. {Circle.getVersion()}");
+            Console.WriteLine("Booting up...");
+            Circle mainCircle = new Circle();
+            while (true)
+            {
+                if (Console.ReadKey().Key == ConsoleKey.Escape) break;
+            }
+        }
+    }
+}
