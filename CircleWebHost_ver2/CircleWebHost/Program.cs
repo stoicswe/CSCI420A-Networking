8ede125c9683126043f6de29aaa060c1c73ed692
Nathan Bunch <31823043+taranoshi@users.noreply.github.com>
2018-05-15 08:29:59 -0400

    added project 2 version 2
    this is a second version of the 2nd project, since the first version needed some re-designing

---
+++ CircleWebHost_ver2/CircleWebHost/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+/*============================== About ==============================
+ * Author: Nathan Bunch
+ * Date: 3/11/2018
+ * Version: 1.11
+ * 
+ * Description: The Program class runs and initializes various circle
+ * sub-classes within the namespace: CircleWebHost. Eventually the
+ * webserver should be able to host more than one website at a time.
+ * 
+ * License: GNU GPL v3
+ * 
+ * Note: Made for a personal project / educational submission
+ * */
+
+/*
+ * 
+ * Changelog:
+ * 
+ * 3/11/2018 - version 1.0: generated the base code (main function)
+ * 3/11/2018 - version 1.1: beep added
+ * 3/11/2018 - version 1.11: removed beep
+ * 3/14/2018 - version 1.12: chnaged "Circle Master Head" to "Circle Sever" --because less edgy.......
+ * */
+
+namespace CircleWebHost
+{
+    class Program
+    {
+
+        private static double version = 1.12;
+
+        static void Main(string[] args)
+        {
+            string circle = @"
+ ██████╗██╗██████╗  ██████╗██╗     ███████╗    ██╗    ██╗███████╗██████╗ 
+██╔════╝██║██╔══██╗██╔════╝██║     ██╔════╝    ██║    ██║██╔════╝██╔══██╗
+██║     ██║██████╔╝██║     ██║     █████╗      ██║ █╗ ██║█████╗  ██████╔╝
+██║     ██║██╔══██╗██║     ██║     ██╔══╝      ██║███╗██║██╔══╝  ██╔══██╗
+╚██████╗██║██║  ██║╚██████╗███████╗███████╗    ╚███╔███╔╝███████╗██████╔╝
+ ╚═════╝╚═╝╚═╝  ╚═╝ ╚═════╝╚══════╝╚══════╝     ╚══╝╚══╝ ╚══════╝╚═════╝ ";
+            Console.WriteLine(circle);
+            Console.WriteLine($"Welcome to Circle WebHost Ver. {version}");
+            Console.WriteLine($"Circle Server Ver. {Circle.getVersion()}");
+            Console.WriteLine("Booting up...");
+            Circle mainCircle = new Circle();
+            while (true)
+            {
+                if (Console.ReadKey().Key == ConsoleKey.Escape) break;
+            }
+        }
+    }
+}
